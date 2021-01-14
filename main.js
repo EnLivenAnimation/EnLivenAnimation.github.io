7937876a9204b68394d79e685ca7d3807a8b8b1e
Wonjong Park <Wonjong@hyuns-mbp.home>
2021-01-13 21:29:50 -0500

    Justin sucks

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ console.log("Test File");
 
 console.log("Hello");
 
-<<<<<<< HEAD
 console.log("bruh");
-=======
+
 console.log("wassup")
 console.log("Grunt");
 console.log("i am cool");
 console.log("I AM BETTER THAN EDWARD")
->>>>>>> ff81d6467c3925a1447a07ee504203219c7fc844
+
+console.log(4);
