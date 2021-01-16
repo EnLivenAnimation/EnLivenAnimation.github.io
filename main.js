d33f2fea78681830469bbda79593cf9e61d37869
Justin <justin82049@gmail.com>
2021-01-16 11:48:03 -0500

    object literals testing

--- main.js
+++ main.js
@@ -9,3 +9,10 @@ console.log("i am cool");
 console.log("I AM BETTER THAN EDWARD")
 
 console.log(4);
+const people = {
+    idiot1: "Edward",
+    idiot2: "Wonjong"
+}
+const {idiot1, idiot2} = people;
+console.log(idiot2);
+console.log(people);
