b4bb53a40bf75bbe0ff42fab3b63091ebe863dee
Justin <justin82049@gmail.com>
2021-01-16 11:43:17 -0500

    object literal testing

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ console.log("Test File");
 
 console.log("Hello");
 
-<<<<<<< HEAD
 console.log("bruh");
-=======
 console.log("wassup")
 console.log("Grunt");
 console.log("i am cool");
 console.log("I AM BETTER THAN EDWARD")
->>>>>>> ff81d6467c3925a1447a07ee504203219c7fc844
+const people = {
+    idiot: "edward",
+    idiot2: "wonjong"
+}
+const { idiot, idiot2} = people;
+console.log(idiot)
