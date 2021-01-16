6d28062a24f6a641e67125a7714799c8ea3f8e1b
Justin <justin82049@gmail.com>
2021-01-16 11:44:34 -0500

    merge conflict stuff

--- main.js
+++ main.js
@@ -7,9 +7,3 @@ console.log("wassup")
 console.log("Grunt");
 console.log("i am cool");
 console.log("I AM BETTER THAN EDWARD")
-const people = {
-    idiot: "edward",
-    idiot2: "wonjong"
-}
-const { idiot, idiot2} = people;
-console.log(idiot)
