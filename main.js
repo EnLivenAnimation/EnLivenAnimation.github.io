3a00175af7764a3178ee21e9f7e201ddeccde1dd
Wonjong Park <Wonjong@hyuns-mbp.home>
2021-01-17 17:04:17 -0500

    add const objects

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ console.log("wassup")
 console.log("Grunt");
 console.log("i am cool");
 console.log("I AM BETTER THAN EDWARD")
+console.log("Robotics");
+
 
 console.log(4);
 const people = {
@@ -16,3 +18,17 @@ const people = {
 const {idiot1, idiot2} = people;
 console.log(idiot2);
 console.log(people);
+const anime ={
+    justin: 'miku',
+    wonjong: 'yukinoshita',
+    edward: 'santa',
+    eric: ' hamster'
+}
+console.log(anime);
+
+const eds =[
+    {bruh: 'edward'},
+    {humph: 'edward'}
+]
+const edJSON = JSON.stringify(eds);
+console.log(edJSON);
