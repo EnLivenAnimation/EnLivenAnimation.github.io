da7169af55b67aff8324cc8ad0953de2d40c9612
ericwang13 <ericwang13@protonmail.com>
2021-01-23 22:26:07 -0500

    Setup image loading in pixi,js

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-let type = "WebGL"
-if(!PIXI.utils.isWebGLSupported()){
-  type = "canvas"
-}
-
-PIXI.utils.sayHello(type)
-
-//Create a Pixi Application
-let app = new PIXI.Application({width: 256, height: 256});
-
-//Add the canvas that Pixi automatically created for you to the HTML document
+let app = new PIXI.Application({ width: 4500, height: 2500 });
 document.body.appendChild(app.view);
+app.renderer.backgroundColor = 0xff0000;
+
+//load an image and run the `setup` function when it's done
+PIXI.loader
+  .add("res/img/f16.jpg")
+  .load(setup);
+
+//This `setup` function will run when the image has loaded
+function setup() {
+
+  //Create the cat sprite
+  let cat = new PIXI.Sprite(PIXI.loader.resources["res/img/f16.jpg"].texture);
+
+  //Add the cat to the stage
+  app.stage.addChild(cat);
+}
