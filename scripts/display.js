3dd217fd890f951e3cce0a4148251250f081159a
LyfeMaster <64602623+LyfeMaster@users.noreply.github.com>
2021-06-02 16:55:23 -0400

    Updated visual values to fit origin at bottomleft

--- scripts/display.js
+++ scripts/display.js
@@ -1,7 +1,7 @@
 function displaySprite(texture, visualX, visualY, visualWidth, visualHeight, tint, parent) {
   DisplayedSprite = new Sprite(texture);
   DisplayedSprite.x = visualX;
-  DisplayedSprite.y = visualY;
+  DisplayedSprite.y = -1*visualY+app.screen.height;
   DisplayedSprite.visualX = visualX;
   DisplayedSprite.visualY = visualY;
   DisplayedSprite.originalWidth = DisplayedSprite.width;
@@ -14,7 +14,9 @@ function displaySprite(texture, visualX, visualY, visualWidth, visualHeight, tin
   DisplayedSprite.interactive = true;
   DisplayedSprite.buttonMode = true;
   DisplayedSprite.anchor.set(0.5);
-  DisplayedSprite.tint = tint;
+  if (tint != null){
+      DisplayedSprite.tint = tint;
+  }
   DisplayedSprite.timeline = [];
   DisplayedSprite
     .on('pointerdown', onDragStartSprite)
@@ -24,20 +26,31 @@ function displaySprite(texture, visualX, visualY, visualWidth, visualHeight, tin
     .on('click', enableButtons);
   DisplayedSprite.spriteChildren = [];
 
+  DisplayedSprite.deleted = false;
+
   app.stage.addChild(DisplayedSprite);
   allSprites.push(DisplayedSprite);
 
   DisplayedSprite.directParent = null;
   if (parent != null) {
-    parent.addChild(DisplayedSprite);
-    parent.spriteChildren.push(DisplayedSprite);
+      parent.addChild(DisplayedSprite);
+      parent.spriteChildren.push(DisplayedSprite);
 
-    DisplayedSprite.x = (visualX - parent.visualX)*parent.originalWidth/parent.visualWidth;
-    DisplayedSprite.y = (visualY - parent.visualY)*parent.originalHeight/parent.visualHeight;
-    DisplayedSprite.width = visualWidth*parent.originalWidth/parent.visualWidth;
-    DisplayedSprite.height = visualHeight*parent.originalHeight/parent.visualHeight;
+      DisplayedSprite.x = (visualX - parent.visualX)*parent.originalWidth/parent.visualWidth;
+      // console.log(app.screen.height);
+      // console.log(parent.visualY);
+      // console.log(DisplayedSprite.visualY);
+      DisplayedSprite.y = (-1*parent.visualY+app.screen.height-visualY) * parent.originalHeight / parent.visualHeight;
+      DisplayedSprite.width = visualWidth*parent.originalWidth/parent.visualWidth;
+      DisplayedSprite.height = visualHeight*parent.originalHeight/parent.visualHeight;
 
-    DisplayedSprite.directParent = parent;
+      DisplayedSprite.directParent = parent;
+  }
+
+  if (timelineLength > 0){
+      for (i = 0; i < timelineLength; i++){
+          DisplayedSprite.timeline.push([-1]);
+      }
   }
 
   let Buttontexture = PIXI.Texture.from("res/img/square.jpg");
@@ -59,42 +72,42 @@ function displaySprite(texture, visualX, visualY, visualWidth, visualHeight, tin
   DisplayedSprite.n = n;
   xs = [-m, 0, m, m, m, 0, -m, -m, 0];
   ys = [-n, -n, -n, 0, n, n, n, 0, -n*(1+40/DisplayedSprite.originalHeight)];
-    
+  
   for (i in DisplayedSprite.buttonArray) {
-    currButton = DisplayedSprite.buttonArray[i];
-    DisplayedSprite.addChild(currButton);
-    currButton.anchor.set(0.5);
-    currButton.interactive = true;
-    currButton.buttonMode = true;
-    currButton.x = xs[i];
-    currButton.y = ys[i];
-    currButton.tint = 0x1a73e8;
+      currButton = DisplayedSprite.buttonArray[i];
+      DisplayedSprite.addChild(currButton);
+      currButton.anchor.set(0.5);
+      currButton.interactive = true;
+      currButton.buttonMode = true;
+      currButton.x = xs[i];
+      currButton.y = ys[i];
 
-    currButton
-      .on('pointerdown', onDragStartButton)
-      .on('pointerup', onDragEndButton)
-      .on('pointerupoutside', onDragEndButton);
+      currButton
+          .on('pointerdown', onDragStartButton)
+          .on('pointerup', onDragEndButton)
+          .on('pointerupoutside', onDragEndButton);
   }
 
   DisplayedSprite.buttonArray[0]
-    .on('pointermove', B1);
+      .on('pointermove', B1);
   DisplayedSprite.buttonArray[1]
-    .on('pointermove', B2);
+      .on('pointermove', B2);
   DisplayedSprite.buttonArray[2]
-    .on('pointermove', B3);
+      .on('pointermove', B3);
   DisplayedSprite.buttonArray[3]
-    .on('pointermove', B4);
+      .on('pointermove', B4);
   DisplayedSprite.buttonArray[4]
-    .on('pointermove', B5);
+      .on('pointermove', B5);
   DisplayedSprite.buttonArray[5]
-    .on('pointermove', B6);
+      .on('pointermove', B6);
   DisplayedSprite.buttonArray[6]
-    .on('pointermove', B7);
+      .on('pointermove', B7);
   DisplayedSprite.buttonArray[7]
-    .on('pointermove', B8);
+      .on('pointermove', B8);
   DisplayedSprite.buttonArray[8]
-    .on('pointermove', BRotate);
+      .on('pointermove', BRotate);
 
   resizeButtons(DisplayedSprite);
+  disableButtons();
   return DisplayedSprite;
 }
