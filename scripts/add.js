e42aea22dfb293ecfda736b3e137747707147c73
LyfeMaster <64602623+LyfeMaster@users.noreply.github.com>
2021-06-03 12:05:52 -0400

    test

--- scripts/add.js
+++ scripts/add.js
@@ -17,7 +17,7 @@ function addCircle(){
 
 function addSquare(){
     let squaretexture = PIXI.Texture.from("res/img/square.jpg");
-    displaySprite(squaretexture, 200, 200, 120, 120, null, null);
+    displaySprite(squaretexture, 200, 200, 120, 120, 0xcccc00, null);
 }
 
 function addTriangle(){
