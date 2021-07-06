4a0bf4faa9c93b80cd8cd28cdd7a9f57524e742f
LyfeMaster <64602623+LyfeMaster@users.noreply.github.com>
2021-07-05 21:04:53 -0400

    Tmp

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -231,6 +231,36 @@ function disableButtons(eventData) {
 
 }
 
+function findObjectCoords(mouseEvent){
+    var obj = document.getElementById("objectBox");
+    var obj_left = 0;
+    var obj_top = 0;
+    var xpos;
+    var ypos;
+    while (obj.offsetParent)
+    {
+        obj_left += obj.offsetLeft;
+        obj_top += obj.offsetTop;
+        obj = obj.offsetParent;
+    }
+    if (mouseEvent)
+    {
+        //FireFox
+        xpos = mouseEvent.pageX;
+        ypos = mouseEvent.pageY;
+    }
+    else
+    {
+        //IE
+        xpos = window.event.x + document.body.scrollLeft - 2;
+        ypos = window.event.y + document.body.scrollTop - 2;
+    }
+    xpos -= obj_left;
+    ypos -= obj_top;
+    document.getElementById("objectCoords").innerHTML = xpos + ", " + ypos;
+}
+document.getElementById("objectBox").onmousemove = findObjectCoords;
+
 let canParentMove = false;
 { // ON DRAG START STUFF
     function onDragStartButton(event) {
