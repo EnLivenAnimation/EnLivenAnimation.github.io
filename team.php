16a3dd3ced7fd0033b985504f6ac41f7c85cfeb3
Wonjong Park <Wonjong@Hyuns-MacBook-Pro.local>
2021-07-11 22:25:51 -0400

    login version 1.0

---
+++ team.php
@@ -0,0 +1,21 @@
+<?php
+    include_once 'header.php'
+?>
+<link rel="stylesheet" href="style/team.css">
+<div id="team-page">
+    <br>
+    <h2 style='text-align:center'>Meet our Founding Members!</h2>
+    <div id="team-info">
+    <div id="justin">
+        <div class="justin-img">
+            <h2>Hello</h2>
+        </div>
+    </div>
+    <h2>hi</h2>
+    
+    </div>
+</div>
+
+<?php
+    include_once 'footer.php'
+?>
