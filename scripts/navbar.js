90d49d6a057250337041796682bc8fdf8c13c0f6
Wonjong Park <Wonjong@Hyuns-MacBook-Pro.local>
2021-07-31 22:58:04 -0400

    fix hero text

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -4,26 +4,49 @@ window.onscroll = () => {
   const b2 = document.querySelector('#b2');
   const b3 = document.querySelector('#b3');
   const b4 = document.querySelector('#b4');
+  const hero1 = document.querySelector('#hero1');
+  const hero2 = document.querySelector('#hero2');
   if(this.scrollY <= 10) {
     nav.className = '' ;
     b1.className = '';
     b2.className = '';
     b3.className = '';
     b4.className = '';
+    hero1.className = '';
+  }
+  else if(this.scrollY <= 1050){
+    hero2.className='';
+  }
+  else if(this.scrollY <= 1100){
+    hero1.className='';
+    hero2.className ='scroll';
   }
   else if(this.scrollY <= 1400){
+    
     nav.className = 'scroll';
     b1.className = 'scroll';
     b2.className = 'scroll';
     b3.className = 'scroll';
     b4.className = 'scroll';
+    hero1.className='scroll';
+    hero2.className ='scroll';
 
   }
+
+  else if(this.scrollY <= 1650){
+    hero2.className='scroll';
+  }
+  else if(this.scrollY <= 1700){
+    hero1.className='scroll';
+    hero2.className ='scroll2';
+  }
+
   else {
-    nav.className = 'scroll';
     b1.className = 'scroll2';
     b2.className = 'scroll2';
     b3.className = 'scroll2';
     b4.className = 'scroll2';
+    hero1.className='scroll2';
+    hero2.className ='scroll2';
   }
 };
