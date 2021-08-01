45ac9ac55e8477d1eb830bc01071d3ac0d71a26f
LyfeMaster <64602623+LyfeMaster@users.noreply.github.com>
2021-07-31 22:06:36 -0400

    tmp

--- scripts/undo.js
+++ scripts/undo.js
@@ -1,3 +1,14 @@
-let allFunctionsArray = [
+let undoStack = [];
+
+function addToUndoStack(functionID, othervars){
+    undoStack.push([functionID, othervars]); 
+    // othervars is an array
+}
+
+function undo(){
+    
+}
+
+let allFunctions = [
     
 ]
