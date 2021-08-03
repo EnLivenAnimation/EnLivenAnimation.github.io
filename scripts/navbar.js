d7609b5c78378d4f7cf5a381c5849448b24d3d04
Wonjong Park <Wonjong@Hyuns-MacBook-Pro.local>
2021-08-02 20:52:45 -0400

    fix hero text

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -6,47 +6,30 @@ window.onscroll = () => {
   const b4 = document.querySelector('#b4');
   const hero1 = document.querySelector('#hero1');
   const hero2 = document.querySelector('#hero2');
-  if(this.scrollY <= 10) {
+  if(this.scrollY <= 1100) {
     nav.className = '' ;
     b1.className = '';
     b2.className = '';
     b3.className = '';
     b4.className = '';
     hero1.className = '';
+    hero2.className ='';
   }
-  else if(this.scrollY <= 1050){
-    hero2.className='';
-  }
-  else if(this.scrollY <= 1100){
-    hero1.className='';
+  else if(this.scrollY<=1500){
+    nav.className = '' ;
+    b1.className = '';
+    b2.className = '';
+    b3.className = '';
+    b4.className = '';
+    hero1.className = 'scroll';
     hero2.className ='scroll';
-  }
-  else if(this.scrollY <= 1400){
-    
-    nav.className = 'scroll';
-    b1.className = 'scroll';
-    b2.className = 'scroll';
-    b3.className = 'scroll';
-    b4.className = 'scroll';
-    hero1.className='scroll';
-    hero2.className ='scroll';
-
-  }
-
-  else if(this.scrollY <= 1650){
-    hero2.className='scroll';
-  }
-  else if(this.scrollY <= 1700){
-    hero1.className='scroll';
-    hero2.className ='scroll2';
-  }
-
-  else {
+  }  
+  else if(this.scrollY>=1350){
+    nav.className='scroll';
     b1.className = 'scroll2';
     b2.className = 'scroll2';
     b3.className = 'scroll2';
     b4.className = 'scroll2';
-    hero1.className='scroll2';
-    hero2.className ='scroll2';
+    hero2.className ='scroll';
   }
 };
