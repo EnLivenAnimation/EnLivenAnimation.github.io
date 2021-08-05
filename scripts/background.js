d19bcfd94ee2455cfe4c3bbbb97262461da6d436
zou-Justin <58957203+zou-Justin@users.noreply.github.com>
2021-08-04 21:15:19 -0400

    Create background.js

---
+++ scripts/background.js
@@ -0,0 +1,9 @@
+const gg = PIXI.Texture.from("res/img/hero.jpeg");
+
+
+
+function addBackground(background1,wantedTexture){
+    background1.alpha = 1;
+    background1.texture = wantedTexture;
+    console.log("DOIN STUF");
+  }
