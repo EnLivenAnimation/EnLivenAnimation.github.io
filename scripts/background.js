8a62cc39a8ff9ca0c5fe0464fa5d69863b1c21e6
zou-Justin <58957203+zou-Justin@users.noreply.github.com>
2021-08-10 14:38:34 -0400

    different background images

--- scripts/background.js
+++ scripts/background.js
@@ -1,9 +1,16 @@
 const gg = PIXI.Texture.from("res/img/hero.jpeg");
-
-
-
+const hg = PIXI.Texture.from("res/img/blackBackground.jpg");
+const bg = PIXI.Texture.from("res/img/background.jpg");
 function addBackground(background1,wantedTexture){
     background1.alpha = 1;
     background1.texture = wantedTexture;
-    console.log("DOIN STUF");
   }
+function addBlackBackground(){
+  addBackground(background,hg)
+}
+function addDefault(){
+  addBackground(background,bg)
+}
+function addYourName(){
+  addBackground(background,gg)
+}
