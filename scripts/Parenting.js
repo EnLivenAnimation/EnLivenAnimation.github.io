b053fa204d398ec426687ed47cd1ba616bc9c04f
Justin <justin82049@gmail.com>
2021-08-21 14:20:03 -0400

    parenting

--- scripts/Parenting.js
+++ scripts/Parenting.js
@@ -13,33 +13,25 @@ function ParentToChild(child,parent){
     child.y = (-1*parent.visualY+app.screen.height-child.visualY) * parent.originalHeight / parent.visualHeight;
     child.width = child.visualWidth*parent.originalWidth/parent.visualWidth;
     child.height = child.visualHeight*parent.originalHeight/parent.visualHeight;
-
     resizeButtons(child);
 }
 
 function removeParent(child,parent){
-    if (child.parent == parent){
+    if (child.directParent == parent){
+        console.log("SOMETHING");
         child.x = (child.visualX - parent.visualX)*parent.originalWidth/parent.visualWidth;
         child.y = (-1*parent.visualY+app.screen.height-child.visualY) * parent.originalHeight / parent.visualHeight;
-        console.log(child.visualX + "SDJSK");
+        // console.log(child.visualX + "SDJSK");
         child.width = child.visualWidth*parent.originalWidth/parent.visualWidth;
         child.height = child.visualHeight*parent.originalHeight/parent.visualHeight;
-        let newChild = displaySprite(child.Texture,child.visualX,child.visualY,child.width,child.height,null,null);
-        // newChild.x = child.x;
-        // newChild.y = child.y;
-        // newChild.visualX = child.visualX;
-        // newChild.visualY = child.visualY;
-        // newChild.height =  child.height;
-        // newChild.width =  child.width;
-        // selectedSprite = child;
-        // DeleteSprite();
-        newChild.alpha = 0;
-        newChild.interactive = false;
-        parent.removeChild(child);
-        allSprites.splice(0,1);
-        // newChild.x = 240;
-        // newChild.y = 240;
-        newChild.alpha = 1;
-        newChild.interactive = true;
+        child.parent = null;
+        child.directParent = null;
+        // let newChild = displaySprite(child.Texture,child.visualX,child.visualY,child.width,child.height,null,null);
+        // newChild.alpha = 0;
+        // newChild.interactive = false;
+       
+        // allSprites.splice(0,1);
+        // newChild.alpha = 1;
+        // newChild.interactive = true;
     }
 }
